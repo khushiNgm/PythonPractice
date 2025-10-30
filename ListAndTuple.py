a36453dae3f0e2f9e3524c2da2980ebeef63df9e
khushiN <nigamkhushi731@gmail.com>
2025-10-30 22:54:12 +0530

    List&Tuple learn

---
+++ ListAndTuple.py
@@ -0,0 +1,21 @@
+# Question1: WAP to ask the user to enter names of their 3 favorite movie & store them in a list
+movies= []
+mov1 = input("Enter your fav mov1: " )
+mov2 = input("Enter your fav mov2: ")
+mov3 = input("Enter your fav mov3: ")
+
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
+
+moives.append(input("Enter your fav mov1: " ))
+
+# TYPE2:
+'''
+moives.append(input("Enter your fav mov1: " ))
+moives.append(input("Enter your fav mov2: " ))
+moives.append(input("Enter your fav mov3: " )) 
+
+'''
